ade9c0e2ec1b501e87dd86994d7ceff3bac65f24
wangjian <380018931@qq.com>
2018-07-02 17:50:07 +0800

    修改bug

--- src/test/java/com/selenium_day01/TestNgDemo1.java
+++ src/test/java/com/selenium_day01/TestNgDemo1.java
@@ -20,7 +20,7 @@ public class TestNgDemo1 {
     public void testCase1(){
         System.out.println("Hello,World!");
         String a = "Hello,World!";
-        String b = "hello,world!";
+        String b = "Hello,World!";
         Assert.assertEquals(a,b);
     }
 
